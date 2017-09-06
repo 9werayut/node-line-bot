01811d41588b16f91f8b9f089f3d2372d9698624
werayut.ku@gmail.com <werayut.ku@gmail.com>
2017-09-06 10:52:05 +0700

    debug message

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/webhook', (req, res) => {
   var replyToken = req.body.events[0].replyToken
   console.log(text, sender, replyToken)
   console.log(typeof sender, typeof text)
-  // console.log(req.body.events[0])
+  console.log(req.body.events[0])
   if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
     sendText(sender, text);
   }
